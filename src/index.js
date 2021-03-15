86d00520fa7efc1f2ac4f11029fcb2efd6eba804
Мария Светлая <merry76light@gmail.com>
2021-03-16 00:55:37 +0300

    feat: add brackets 2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  let open = ['(', '[', '{', '|'];
-  let close = [')', ']', '}', '|'];
+  let open = ['(', '[', '{', '|', '1', '3', '5', '7', '8'];
+  let close = [')', ']', '}', '|','2', '4', '6', '7', '8'];
   let indexOpen;
   let indexClose;
   let stack = [];
 
   for (let i=0; i<str.length; i++){
     indexOpen = open.indexOf(str[i]);
-    if (indexOpen !== -1){
+    if (indexOpen == 3){
+      stack.push(indexOpen);
+    }
+    else if (indexOpen !== -1 && indexOpen !== 3){
       stack.push(indexOpen);
       continue;
     }
     indexClose = close.indexOf(str[i]);
-    if (indexClose !== -1){
+      if (indexClose !== -1){
       indexOpen = stack.pop();
       if (indexClose !== indexOpen){
         return false;
@@ -22,5 +25,5 @@ module.exports = function check(str, bracketsConfig) {
     if (stack.length !== 0){
       return false;
     }
-  return true;
+    return true;
 }
